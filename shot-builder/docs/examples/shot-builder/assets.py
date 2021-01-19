e7899b10886fd1273bd245c86b8781df1e16cda0
Jeroen Bakker <jeroen@blender.org>
2021-01-19 17:21:31 +0100

    Updated the example configuration file

--- shot-builder/docs/examples/shot-builder/assets.py
+++ shot-builder/docs/examples/shot-builder/assets.py
@@ -1,8 +1,12 @@
 from shot_builder.asset import AssetConfig
 
 
-class Character(AssetConfig):
-    path = "{production.path}/lib/char/{asset.config.asset_code}/{asset.config.asset_code}.blend"
+class Asset(AssetConfig):
+    path = "{production.path}/lib/{asset.config.asset_type}/{asset.config.asset_code}/{asset.config.asset_code}.blend"
+
+
+class Character(Asset):
+    asset_type = "char"
     collection = "CH-{asset.config.asset_code}"
 
 
@@ -29,3 +33,48 @@ class Rex(Character):
 class Jay(Character):
     asset_name = "Jay"
     asset_code = "jay"
+
+
+# class Bird(Character):
+#     asset_name = "Bird"
+#     asset_code = "bird"
+
+
+class Prop(Asset):
+    asset_type = "props"
+    collection = "PR-{asset.config.asset_code}"
+
+
+class Boombox(Prop):
+    asset_name = "Boombox"
+    asset_code = "boombox"
+
+
+class BBQGrill(Prop):
+    asset_name = "BBQ Grill"
+    asset_code = "bbq_grill"
+
+
+class NotepadAndPencil(Prop):
+    asset_name = "Notepad and pencil"
+    asset_code = "notepad_pencil"
+
+
+class Binoculars(Prop):
+    asset_name = "Binoculars (Ellie)"
+    asset_code = "binoculars"
+
+
+class Backpack(Prop):
+    asset_name = "Backpack (Phil)"
+    asset_code = "backpack"
+
+
+class Set(Asset):
+    asset_type = "sets"
+    collection = "SE-{asset.config.asset_code}"
+
+
+class MushroomGrove(Set):
+    asset_name = "Mushroom grove"
+    asset_code = "mushroom_grove"
