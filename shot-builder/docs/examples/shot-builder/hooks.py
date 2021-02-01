c3ebae7698310ec8ae089c7c097d94df64326d2f
Jeroen Bakker <jeroen@blender.org>
2021-02-01 08:57:38 +0100

    Added output collection for anim files.
    During sprite fright the output collection is used to link animation
    data to the lighting file.

--- shot-builder/docs/examples/shot-builder/hooks.py
+++ shot-builder/docs/examples/shot-builder/hooks.py
@@ -1,6 +1,7 @@
 import bpy
 from shot_builder.hooks import hook, Wildcard
 from shot_builder.asset import Asset
+from shot_builder.shot import Shot
 
 
 @hook()
@@ -13,17 +14,31 @@ def task_type_anim_set_workbench(scene: bpy.types.Scene, **kwargs):
     scene.render.engine = 'BLENDER_WORKBENCH'
 
 
+@hook(match_task_type='anim')
+def task_type_anim_output_collection(shot: Shot, task_type: str, **kwargs):
+    """
+    Animations are stored in an output collection. This collection will be linked
+    by the lighting file.
+    """
+    output_collection = bpy.data.collections.new(
+        name=f"{shot.sequence_code}_{shot.code}.{task_type}.output")
+    shot.output_collection = output_collection
+
+
 @hook(match_task_type='anim', match_asset_type=['char', 'props'])
-def link_char_prop_for_anim(scene: bpy.types.Scene, asset: Asset, **kwargs):
-    bpy.ops.wm.link(
-        filepath=str(asset.path),
-        directory=str(asset.path) + "/Collection",
-        filename=asset.collection,
-    )
+def link_char_prop_for_anim(shot: Shot, asset: Asset, **kwargs):
+    if asset.collection not in bpy.data.collections:
+        bpy.ops.wm.link(
+            filepath=str(asset.path),
+            directory=str(asset.path) + "/Collection",
+            filename=asset.collection,
+        )
+    asset_collection = bpy.data.collections[asset.collection]
+    shot.output_collection.children.link(asset_collection)
 
 
 @hook(match_task_type=Wildcard, match_asset_type='sets')
-def link_set(scene: bpy.types.Scene, asset: Asset, **kwargs):
+def link_set(asset: Asset, **kwargs):
     bpy.ops.wm.link(
         filepath=str(asset.path),
         directory=str(asset.path) + "/Collection",
