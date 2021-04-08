7adcdf1f4380fa3b36693d89680b1d8154ffe28d
Paul Golter <paulgolter.de@gmail.com>
2021-04-08 15:34:54 +0200

    initial commit

---
+++ cache_manager/__init__.py
@@ -0,0 +1,45 @@
+import bpy
+from . import ops
+from . import ui
+from .logger import LoggerFactory
+
+logger = LoggerFactory.getLogger(__name__)
+
+bl_info = {
+    "name": "Cache Manager",
+    "author": "Paul Golter",
+    "description": "Blender addon to streamline alembic caches of assets",
+    "blender": (2, 93, 0),
+    "version": (0, 1, 0),
+    "location": "View3D",
+    "warning": "",
+    "doc_url": "",
+    "tracker_url": "",
+    "category": "Generic",
+}
+
+_need_reload = "ops" in locals()
+
+if _need_reload:
+    import importlib
+
+    logger.info("-START- Reloading Cache Manager")
+    logger.info("-END- Reloading Cache Manager")
+
+
+def register():
+    logger.info("-START- Registering Cache Manager")
+    ops.register()
+    ui.register()
+    logger.info("-END- Registering Cache Manager")
+
+
+def unregister():
+    logger.info("-START- Unregistering Cache Manager")
+    ui.unregister()
+    ops.unregister()
+    logger.info("-END- Unregistering Cache Manager")
+
+
+if __name__ == "__main__":
+    register()
