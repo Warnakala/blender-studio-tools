01a2356d4d2886f7707774bf51213fd9f333f963
Paul Golter <paulgolter.de@gmail.com>
2021-04-29 17:42:58 +0200

    add CAM_DATA_PATHS to cmglobals

--- cache_manager/cmglobals.py
+++ cache_manager/cmglobals.py
@@ -22,3 +22,24 @@ DRIVER_VIS_DATA_PATHS: List[str] = [
     "show_viewport",
     "show_render",
 ]
+
+CAM_DATA_PATHS: List[str] = [
+    "angle",
+    "angle_x",
+    "angle_y",
+    "clip_end",
+    "clip_start",
+    "display_size",
+    "dof.aperture_blades",
+    "dof.aperture_fstop",
+    "dof.aperture_ratio",
+    "dof.aperture_rotation",
+    "lens",
+    "lens_unit",
+    "ortho_scale",
+    "sensor_fit",
+    "sensor_height",
+    "sensor_width",
+    "shift_x",
+    "shift_y",
+]
