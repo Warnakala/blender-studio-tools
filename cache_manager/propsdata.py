8bc0913a4c835a66ddb23668f35cf53416cda4e1
Paul Golter <paulgolter.de@gmail.com>
2021-05-04 18:20:37 +0200

    rm_deleted_colls_from_list removes indeces in reverse, works now as excpected

--- cache_manager/propsdata.py
+++ cache_manager/propsdata.py
@@ -120,34 +120,35 @@ def get_cache_version_dir_path_str(self: Any) -> str:
 
 
 def rm_deleted_colls_from_list(context: bpy.types.Context) -> None:
-    return None
 
     for category in [context.scene.cm.colls_export, context.scene.cm.colls_import]:
 
         category_name = context.scene.cm.category
         colls = [item.coll_ptr for item in category]
+        colls.reverse()
 
+        # make sure to remove list from the back to not throw off the subsequent indexes.
         for idx, coll in enumerate(colls):
+            org_idx = len(colls) - 1 - idx
             if not coll:
                 # remove item for that category at that index
-                category.remove(idx)
+                category.remove(org_idx)
                 logger.info(
                     "Removed index %i from %s list. Does not exists anymore.",
-                    idx,
+                    org_idx,
                     category_name.lower(),
                 )
                 # update selection index
-                index = context.scene.cm.colls_export_index
+                curr_index = context.scene.cm.colls_export_index
                 if category_name == "IMPORT":
-                    index = context.scene.cm.colls_import_index
+                    curr_index = context.scene.cm.colls_import_index
 
-                if index == idx and index > 0:
+                if curr_index == org_idx and curr_index > 0:
 
                     if category_name == "IMPORT":
-                        context.scene.cm.colls_import_index = index - 1
+                        context.scene.cm.colls_import_index = curr_index - 1
                     else:
-                        context.scene.cm.colls_export_index = index - 1
-
+                        context.scene.cm.colls_export_index = curr_index - 1
     ui_redraw()
 
 
