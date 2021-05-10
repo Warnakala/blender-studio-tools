ef390b10a058974277afbdf01c34cbf582b9d93b
Paul Golter <paulgolter.de@gmail.com>
2021-05-10 18:58:46 +0200

    add addon prefs

--- anim_setup/__init__.py
+++ anim_setup/__init__.py
@@ -1,5 +1,6 @@
 import bpy
 
+from . import prefs
 from . import ops
 from . import ui
 from .log import LoggerFactory
@@ -25,6 +26,7 @@ if _need_reload:
     import importlib
 
     logger.info("-START- Reloading anim-setup")
+    prefs = importlib.reload(prefs)
     ops = importlib.reload(ops)
     ui = importlib.reload(ui)
     logger.info("-END- Reloading anim-setup")
@@ -32,6 +34,7 @@ if _need_reload:
 
 def register():
     logger.info("-START- Registering anim-setup")
+    prefs.register()
     ops.register()
     ui.register()
     logger.info("-END- Registering anim-setup")
@@ -41,6 +44,7 @@ def unregister():
     logger.info("-START- Unregistering anim-setup")
     ui.unregister()
     ops.unregister()
+    prefs.unregister()
     logger.info("-END- Unregistering anim-setup")
 
 
---
+++ anim_setup/prefs.py
@@ -0,0 +1,124 @@
+import os
+from pathlib import Path
+from typing import Union, Optional, Any, Dict, Set
+
+import bpy
+
+
+class AS_AddonPreferences(bpy.types.AddonPreferences):
+    bl_idname = __package__
+
+    project_root: bpy.props.StringProperty(  # type: ignore
+        name="Project Root",
+        default="",
+        options={"HIDDEN", "SKIP_SAVE"},
+        subtype="DIR_PATH",
+    )
+    dropbox_root: bpy.props.StringProperty(  # type: ignore
+        name="Dropbox Root",
+        default="",
+        options={"HIDDEN", "SKIP_SAVE"},
+        subtype="DIR_PATH",
+    )
+
+    def draw(self, context: bpy.types.Context) -> None:
+        layout = self.layout
+        box = layout.box()
+        box.row().prop(self, "project_root")
+
+        if not self.project_root:
+            row = box.row()
+            row.label(text="Please specify the project root directory.", icon="ERROR")
+
+        if not bpy.data.filepath and self.project_root.startswith("//"):
+            row = box.row()
+            row.label(
+                text="In order to use a relative path as root cache directory the current file needs to be saved.",
+                icon="ERROR",
+            )
+
+        box.row().prop(self, "dropbox_root")
+
+        if not self.dropbox_root:
+            row = box.row()
+            row.label(text="Please specify the dropbox root directory.", icon="ERROR")
+
+        if not bpy.data.filepath and self.dropbox_root.startswith("//"):
+            row = box.row()
+            row.label(
+                text="In order to use a relative path as dropbox root directory the current file needs to be saved.",
+                icon="ERROR",
+            )
+
+    @property
+    def project_root_path(self) -> Optional[Path]:
+        if not self.is_project_root_valid:
+            return None
+        return Path(os.path.abspath(bpy.path.abspath(self.project_root)))
+
+    @property
+    def is_project_root_valid(self) -> bool:
+
+        # check if file is saved
+        if not self.project_root:
+            return False
+
+        if not bpy.data.filepath and self.project_root.startswith("//"):
+            return False
+
+        return True
+
+    @property
+    def dropbox_root_path(self) -> Optional[Path]:
+        if not self.is_dropbox_root_valid:
+            return None
+        return Path(os.path.abspath(bpy.path.abspath(self.dropbox_root)))
+
+    @property
+    def is_dropbox_root_valid(self) -> bool:
+
+        # check if file is saved
+        if not self.dropbox_root:
+            return False
+
+        if not bpy.data.filepath and self.dropbox_root.startswith("//"):
+            return False
+
+        return True
+
+    @property
+    def is_editorial_valid(self) -> bool:
+        if not self.is_dropbox_root_valid:
+            return False
+
+        return self.dropbox_root_path.joinpath(
+            "shared/sprites/editorial/export"
+        ).exists()
+
+    @property
+    def editorial_path(self) -> Optional[Path]:
+        if not self.is_editorial_valid:
+            return None
+        return Path(self.dropbox_root_path.joinpath("shared/sprites/editorial/export"))
+
+
+def addon_prefs_get(context: bpy.types.Context) -> bpy.types.AddonPreferences:
+    """
+    shortcut to get cache_manager addon preferences
+    """
+    return context.preferences.addons["anim_setup"].preferences
+
+
+# ---------REGISTER ----------
+
+classes = [AS_AddonPreferences]
+
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+
+def unregister():
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
