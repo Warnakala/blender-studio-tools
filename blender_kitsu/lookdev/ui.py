349358f86fcd0a941a88f91afd739e88a8d8ae85
Paul Golter <paulgolter.de@gmail.com>
2021-06-02 16:14:10 +0200

    add frame range error to lookdev tools panel

--- blender_kitsu/lookdev/ui.py
+++ blender_kitsu/lookdev/ui.py
@@ -24,7 +24,10 @@ class KITSU_PT_vi3d_lookdev_tools(bpy.types.Panel):
     @classmethod
     def poll_error(cls, context: bpy.types.Context) -> bool:
         addon_prefs = prefs.addon_prefs_get(context)
-        return not addon_prefs.lookdev.is_presets_dir_valid
+        return bool(
+            context.scene.kitsu_error.frame_range
+            or not addon_prefs.lookdev.is_presets_dir_valid
+        )
 
     def draw_error(self, context: bpy.types.Context) -> None:
         addon_prefs = prefs.addon_prefs_get(context)
@@ -33,6 +36,9 @@ class KITSU_PT_vi3d_lookdev_tools(bpy.types.Panel):
 
         if not addon_prefs.lookdev.is_presets_dir_valid:
             ui.draw_error_invalid_render_preset_dir(box)
+
+        if context.scene.kitsu_error.frame_range:
+            ui.draw_error_frame_range_outdated(box)
 
     def draw(self, context: bpy.types.Context) -> None:
         layout = self.layout
