a7875ae6979e3e155d302870a21b2a6fc406663f
Paul Golter <paulgolter.de@gmail.com>
2021-07-07 17:29:18 +0200

    tests: adjust vis.py to work with new structure

--- tests/vis.py
+++ tests/vis.py
@@ -4,8 +4,8 @@ import pygame
 import colorsys
 from typing import Tuple
 
-RES_X = int(1920 / 2)
-RES_Y = int(1920 / 2)
+RES_X = int(1920 / 1.4)
+RES_Y = int(1080 / 1.4)
 
 
 def get_random_pastel_color_rgb() -> Tuple[float, float, float]:
@@ -19,23 +19,33 @@ def get_random_pastel_color_rgb() -> Tuple[float, float, float]:
     return (int(color[0] * 255), int(color[1] * 255), int(255 * color[2]))
 
 
+def draw_nested_rect(rect):
+    color = get_random_pastel_color_rgb()
+    pygame.draw.rect(
+        surface, color, pygame.Rect(rect.x, rect.y, rect.width, rect.height)
+    )
+    # color = get_random_pastel_color_rgb()
+    fac = 0.5
+    color = (int(color[0] * fac), int(color[1] * fac), int(color[2] * fac))
+    pygame.draw.rect(
+        surface,
+        color,
+        pygame.Rect(rect.child.x, rect.child.y, rect.child.width, rect.child.height),
+    )
+
+
+def draw_single_cell(cell):
+    color = get_random_pastel_color_rgb()
+    pygame.draw.rect(
+        surface, color, pygame.Rect(cell.x, cell.y, cell.width, cell.height)
+    )
+    draw_nested_rect(cell.child)
+
+
 def draw_cells(grid):
     for row_idx in range(grid.row_count()):
         for cell in grid.get_cells_for_row(row_idx):
-            color = get_random_pastel_color_rgb()
-            pygame.draw.rect(
-                surface, color, pygame.Rect(cell.x, cell.y, cell.width, cell.height)
-            )
-            # color = get_random_pastel_color_rgb()
-            fac = 0.5
-            color = (int(color[0] * fac), int(color[1] * fac), int(color[2] * fac))
-            pygame.draw.rect(
-                surface,
-                color,
-                pygame.Rect(
-                    cell.rect.x, cell.rect.y, cell.rect.width, cell.rect.height
-                ),
-            )
+            draw_single_cell(cell)
 
 
 def draw_rows(grid):
@@ -48,7 +58,7 @@ def draw_rows(grid):
 
 
 def draw_colls(grid):
-    for idx, cell in enumerate(grid.colls):
+    for cell in grid.colls:
         color = get_random_pastel_color_rgb()
         print(cell)
         pygame.draw.rect(
@@ -58,25 +68,43 @@ def draw_colls(grid):
 
 # appen sys path
 sys.path.append("/home/guest/dev/projects/render-review/render_review")
-from geo import Grid, Rectangle, Align
+from geo import Grid, Rectangle, NestedRectangle, Align, Cell
 
 
 # Initializing Pygame
 pygame.init()
 surface = pygame.display.set_mode((RES_X, RES_Y))
 
-# draw grid
-grid = Grid(0, 0, RES_X, RES_Y, 4, 6, Rectangle(0, 0, 1920, 1080), align=Align.CENTER)
+nested_rect = NestedRectangle(600, 300, 1920, 1080, child=Rectangle(0, 0, 600, 120))
+cell = Cell(
+    0,
+    0,
+    500,
+    500,
+    child=nested_rect
+    # child=NestedRectangle(100, 100, 400, 400, child=Rectangle(0, 0, 1920, 1080)),
+)
+grid = Grid(0, 0, RES_X, RES_Y, 4, 6, cell)
+
 # draw_rows(grid)
 # draw_colls(grid)
-draw_cells(grid)
 
 # run
+clock = pygame.time.Clock()
 run = True
+draw_cells(grid)
+# draw_single_cell(grid.cells[0][1])
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    # surface.fill(pygame.Color(0, 0, 0, 1))
+    # draw_nested_rect(nested_rect)
+    # draw_single_cell(cell)
     pygame.display.update()
+    # cell.x += 10
+    # cell.child.x += 1
+    # cell.child.child.y += 1
+    clock.tick(4)
 
 pygame.quit()
