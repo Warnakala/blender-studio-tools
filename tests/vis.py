5ae93bfab711a9250d7288f4c67110db4b1090b0
Paul Golter <paulgolter.de@gmail.com>
2021-07-12 12:13:01 +0200

    tests/vis: adjsut drawing method to use color prop on rectangle

--- tests/vis.py
+++ tests/vis.py
@@ -3,6 +3,7 @@ import random
 import pygame
 import colorsys
 from typing import Tuple
+import math
 
 RES_X = int(1920 / 1.4)
 RES_Y = int(1080 / 1.4)
@@ -20,24 +21,35 @@ def get_random_pastel_color_rgb() -> Tuple[float, float, float]:
 
 
 def draw_nested_rect(rect):
-    color = get_random_pastel_color_rgb()
+    if not hasattr(rect, "color"):
+        color = get_random_pastel_color_rgb()
+        rect.color = color
+
     pygame.draw.rect(
-        surface, color, pygame.Rect(rect.x, rect.y, rect.width, rect.height)
+        surface, rect.color, pygame.Rect(rect.x, rect.y, rect.width, rect.height)
     )
     # color = get_random_pastel_color_rgb()
-    fac = 0.5
-    color = (int(color[0] * fac), int(color[1] * fac), int(color[2] * fac))
+    if not hasattr(rect.child, "color"):
+        fac = 0.5
+        color = (
+            int(rect.color[0] * fac),
+            int(rect.color[1] * fac),
+            int(rect.color[2] * fac),
+        )
+        rect.child.color = color
     pygame.draw.rect(
         surface,
-        color,
+        rect.child.color,
         pygame.Rect(rect.child.x, rect.child.y, rect.child.width, rect.child.height),
     )
 
 
 def draw_single_cell(cell):
-    color = get_random_pastel_color_rgb()
+    if not hasattr(cell, "color"):
+        color = get_random_pastel_color_rgb()
+        cell.color = color
     pygame.draw.rect(
-        surface, color, pygame.Rect(cell.x, cell.y, cell.width, cell.height)
+        surface, cell.color, pygame.Rect(cell.x, cell.y, cell.width, cell.height)
     )
     draw_nested_rect(cell.child)
 
@@ -74,45 +86,54 @@ from geo import Grid, Rectangle, NestedRectangle, Align, Cell
 # Initializing Pygame
 pygame.init()
 surface = pygame.display.set_mode((RES_X, RES_Y))
-rect = Rectangle(0, 0, 600, 400)
-nested_rect = NestedRectangle(600, 300, 1920, 1080, child=rect)
+
+# Init Rectangles
+rect = Rectangle(600, 0, 600 / 2, 400 / 2)
+nested_rect = NestedRectangle(
+    600, 300, 1920 / 6, 1080 / 6, child=rect, align=Align.CENTER
+)
 cell = Cell(
     0,
     0,
     500,
     500,
     child=nested_rect,
-    align=Align.CENTER
+    align=Align.TOP
     # child=NestedRectangle(100, 100, 400, 400, child=Rectangle(0, 0, 1920, 1080)),
 )
-grid1 = Grid(0, 0, RES_X, RES_Y, 4, 6, cell_templ=cell, align=Align.CENTER)
-grid1.set_content_scale(0.5)
-grid1.content_scale_y = 1
+grid1 = Grid(0, 0, RES_X, RES_Y, 6, 9, cell_templ=cell, align=Align.CENTER)
+grid1.scale_content(1)
+grid1.scale_content_x(1)
+# grid1.reset_content_transforms()
 # grid1.content_scale_x = 1
 
 content = [NestedRectangle(0, 0, 1920, 1080, child=rect.copy()) for r in range(28)]
-grid2 = Grid.from_content(0, 0, RES_X, RES_Y, content, 3)
-grid2.set_content_scale(0.9)
+grid2 = Grid.from_content(0, 0, RES_X, RES_Y, content, 6)
+grid2.scale_content(0.8)
 # draw_rows(grid)
 # draw_colls(grid)
 
 # run
 clock = pygame.time.Clock()
 run = True
-draw_cells(grid1)
 
 # draw_single_cell(grid.cells[0][1])
+count = 1
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    # surface.fill(pygame.Color(0, 0, 0, 1))
+    surface.fill(pygame.Color(0, 0, 0, 1))
+    # grid2.scale_content(0.5 * (1 + math.sin(2 * 3.14 * 2 * count)) + 0.1)
+    draw_cells(grid1)
     # draw_nested_rect(nested_rect)
+    # nested_rect.scale_x = 1.02
+    # nested_rect.y += 10
     # draw_single_cell(cell)
     pygame.display.update()
     # cell.x += 10
     # cell.child.x += 1
     # cell.child.child.y += 1
     clock.tick(4)
-
+    count += 1
 pygame.quit()
