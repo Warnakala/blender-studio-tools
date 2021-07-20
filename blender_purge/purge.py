702c2f22c2d56055349e56ac20960731cde481f0
Paul Golter <paul@blender.org>
2021-07-20 10:04:20 +0200

    purge.py set preferences.save_version to 0 so no .blend1 files get created

--- blender_purge/purge.py
+++ blender_purge/purge.py
@@ -6,6 +6,9 @@ logger = logging.getLogger(__name__)
 
 import bpy
 
+# setup prefs
+bpy.context.preferences.filepaths.save_version = 0
+
 # purge
 logger.info("Starting Recursive Purge")
 bpy.ops.outliner.orphans_purge(do_local_ids=True, do_linked_ids=True, do_recursive=True)
