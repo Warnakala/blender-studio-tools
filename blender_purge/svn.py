5f3752ef3afd89437016b7f323cc97cad3b98891
Paul Golter <paul@blender.org>
2021-07-20 11:33:57 +0200

    svn: repo add commit function

--- blender_purge/svn.py
+++ blender_purge/svn.py
@@ -1,6 +1,6 @@
 import subprocess
 import os
-from typing import List, Union, Tuple, Any, Dict
+from typing import List, Union, Tuple, Any, Dict, Optional
 from pathlib import Path
 
 
@@ -31,7 +31,7 @@ class SvnRepo:
                 break
         return split
 
-    def get_modified(self) -> List[Path]:
+    def get_modified(self, suffix: str = ".*") -> List[Path]:
         output = self.status()
         if not output:
             return []
@@ -43,14 +43,21 @@ class SvnRepo:
             if not line.startswith("M"):
                 continue
             path = Path(line[5:].strip())
-            path_list.append(path)
+
+            # if no suffix supplied append all files
+            if suffix == ".*":
+                path_list.append(path)
+            # if suffix supplied only collect files that match
+            else:
+                if path.suffix == suffix:
+                    path_list.append(path)
 
         return path_list
 
     def revert(self, relpath_list: List[Path]) -> subprocess.Popen:
-        relpath_str_list = " ".join([p.as_posix() for p in relpath_list])
+        arg_list = " ".join([p.as_posix() for p in relpath_list])
         process = subprocess.Popen(
-            (f"svn revert {relpath_str_list}"), shell=True, cwd=self._path.as_posix()
+            (f"svn revert {arg_list}"), shell=True, cwd=self._path.as_posix()
         )
         return process
 
@@ -58,5 +65,37 @@ class SvnRepo:
         modified = self.get_modified()
         self.revert(modified)
 
-    def commit(self):
-        pass
+    def commit(self, relpath_list: List[Path]) -> Optional[subprocess.Popen]:
+        if not relpath_list:
+            return None
+
+        base = ["svn commit"]
+        base.extend([p.as_posix() for p in relpath_list])
+        print(base)
+        # base.append('-m "test commit')
+        process = subprocess.call(base, shell=True, cwd=self._path.as_posix())
+        return process
+
+    def get_untracked(self) -> List[Path]:
+        output = self.status()
+        if not output:
+            return []
+
+        path_list: List[Path] = []
+
+        # assemble path list
+        for idx, line in enumerate(output):
+            if not line.startswith("?"):
+                continue
+            path = Path(line[5:].strip())
+            path_list.append(path)
+
+        return path_list
+
+
+if __name__ == "__main__":
+    # test status
+    repo = SvnRepo(Path("/media/data/sprites"))
+    modified = repo.get_modified()
+    print(modified)
+    repo.commit(modified)
