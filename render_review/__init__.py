ed88935ee3e15cdf3895d12d591605397702d977
Paul Golter <paul@blender.org>
2021-08-13 14:16:53 +0200

    change bl_info description, rm registering unregistering logs

--- render_review/__init__.py
+++ render_review/__init__.py
@@ -18,10 +18,10 @@ logger = LoggerFactory.getLogger(__name__)
 bl_info = {
     "name": "Render Review",
     "author": "Paul Golter",
-    "description": "Review renders from Flamenco",
+    "description": "Addon to review renders from Flamenco with the Sequence Editor",
     "blender": (3, 0, 0),
     "version": (0, 1, 0),
-    "location": "View3D",
+    "location": "Sequence Editor",
     "warning": "",
     "doc_url": "",
     "tracker_url": "",
@@ -34,8 +34,6 @@ _need_reload = "ops" in locals()
 if _need_reload:
     import importlib
 
-    logger.info("-START- Reloading render-review")
-
     util = importlib.reload(util)
     props = importlib.reload(props)
     prefs = importlib.reload(prefs)
@@ -45,32 +43,23 @@ if _need_reload:
     ops = importlib.reload(ops)
     ui = importlib.reload(ui)
     draw = importlib.reload(draw)
-
-    logger.info("-END- Reloading render-review")
+    logger.info("Reloaded render-review")
 
 
 def register():
-    logger.info("-START- Registering render-review")
-
     props.register()
     prefs.register()
     ops.register()
     ui.register()
     draw.register()
 
-    logger.info("-END- Registering render-review")
-
 
 def unregister():
-    logger.info("-START- Unregistering render-review")
-
     draw.unregister()
     ui.unregister()
     ops.unregister()
     prefs.unregister()
     props.unregister()
-
-    logger.info("-END- Unregistering render-review")
 
 
 if __name__ == "__main__":
