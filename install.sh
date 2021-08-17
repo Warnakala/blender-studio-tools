c0c056e94ddcdd419f59b86870a3a272c2c46ac8
Paul Golter <paul@blender.org>
2021-08-17 09:27:38 +0200

    cleanup: reformat comments install.sh to follow blender-code-style convention

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+NC='\033[0m' # No Color.
 
-#check if pip3 is installed
+# Check if pip3 is installed.
 if ! command -v pip3 &> /dev/null
 then
     echo "Pip3 is not installed"
 
-    #asl user to install pip
+    # Ask user to install pip.
     while true; do
     read -p "Do you wish to install this program? (Yy/Nn)" yn
     case $yn in
@@ -20,17 +20,17 @@ then
 done
 fi
 
-#cd into directory of install.sh script
+# Cd into directory of install.sh script.
 dirpath=`dirname "$0"`
 cd $dirpath
 
-#build wheel
+# Build wheel.
 python3 setup.py bdist_wheel
 
-#install wheel with pip
+# Install wheel with pip.
 pip3 install dist/blender_purge-0.1.0-py2.py3-none-any.whl --user --force-reinstall
 
-#check if PATH variable is correct
+# Check if PATH variable is correct.
 if ! [[ ":$PATH:" == *":$HOME/.local/lib/python3.8/site-packages:"* ]]; then
     printf "\n${RED}\$HOME/.local/lib/python3.8/site-packages is missing in PATH variable\n"
     printf "Please add 'export PATH=\"\$PATH:$HOME/.local/lib/python3.8/site-packages\"' to the file: \$HOME/.profile${NC}\n"
@@ -41,6 +41,6 @@ if ! [[ ":$PATH:" == *":$HOME/.local/bin:"* ]]; then
     printf "Please add 'export PATH=\"\$PATH:$HOME/.local/bin\"' to the file: \$HOME/.profile${NC}\n"
 fi
 
-#log end
+# Log end.
 printf "\n${GREEN}Installed blender-purge. Type 'bpurge' in console to start program!\n"
 printf "To uninstall type 'pip3 uninstall blender-purge'${NC}\n"
