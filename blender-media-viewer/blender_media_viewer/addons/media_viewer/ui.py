9f39092e9867872ce1900ee1abb3e2865fa28d7f
Paul Golter <paul@blender.org>
2021-11-26 12:05:50 +0100

    media-viewer: add check to only draw topbar ui when one media file is selected

--- blender-media-viewer/blender_media_viewer/addons/media_viewer/ui.py
+++ blender-media-viewer/blender_media_viewer/addons/media_viewer/ui.py
@@ -7,12 +7,20 @@ from media_viewer.ops import (
     MV_OT_delete_active_gpencil_frame,
     MV_OT_render_review,
 )
+from media_viewer import ops
 
 
 def MV_TOPBAR_media_viewer(self: Any, context: bpy.types.Context) -> None:
     layout = self.layout
     gpl = context.active_annotation_layer
-    if gpl:
+
+    # Only show annotation settings if there is an actual layer
+    # which should always be the case, safety.
+    # And if filepath_list (represents the selected items in filebrowser)
+    # is one or less. We don't support annotating more than one file as
+    # annotation layers are linked to one media file, so we also can't support
+    # rendering them.
+    if gpl and len(ops.prev_filepath_list) <= 1:
         layout.label(text="Annotation")
         layout.prop(gpl, "color", text="")
 
@@ -21,14 +29,16 @@ def MV_TOPBAR_media_viewer(self: Any, context: bpy.types.Context) -> None:
         )
         layout.operator(MV_OT_delete_all_gpencil_frames.bl_idname, text="", icon="X")
 
-    layout.separator()
-    layout.separator()
-    layout.separator()
-    layout.separator()
-    layout.separator()
-    layout.separator()
-    layout.prop(context.window_manager.media_viewer, "review_output_dir")
-    layout.operator(MV_OT_render_review.bl_idname, icon="RESTRICT_RENDER_OFF", text="")
+        layout.separator()
+        layout.separator()
+        layout.separator()
+        layout.separator()
+        layout.separator()
+        layout.separator()
+        layout.prop(context.window_manager.media_viewer, "review_output_dir")
+        layout.operator(
+            MV_OT_render_review.bl_idname, icon="RESTRICT_RENDER_OFF", text=""
+        )
 
 
 # ----------------REGISTER--------------.
