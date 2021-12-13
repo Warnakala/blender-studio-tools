cd636d831b640ab267c1c7289dbc8f3d6dd02376
Paul Golter <paul@blender.org>
2021-12-13 16:48:03 +0100

    grease-converter: add basic add-on boilerplate code

--- grease-converter/grease_converter/__init__.py
+++ grease-converter/grease_converter/__init__.py
@@ -1,1 +1,58 @@
-__version__ == "0.1.0"
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# ***** END GPL LICENCE BLOCK *****
+#
+# (c) 2021, Blender Foundation
+
+bl_info = {
+    "name": "Grease Pencil Converter",
+    "author": "Paul Golter",
+    "description": "Blender add-on to convert annotations to grease pencil objects and back",
+    "blender": (3, 0, 0),
+    "version": (0, 1, 0),
+    "location": "View3D",
+    "warning": "",
+    "doc_url": "",
+    "tracker_url": "",
+    "category": "Grease Pencil",
+}
+
+from . import (
+    ops,
+    ui
+)
+
+_need_reload = "ops" in locals()
+
+if _need_reload:
+    import importlib
+
+    ops = importlib.reload(ops)
+    ui = importlib.reload(ui)
+
+
+def register():
+    ops.register()
+    ui.register()
+
+def unregister():
+    ui.unregister()
+    ops.unregister()
+
+if __name__ == "__main__":
+    register()
+
---
+++ grease-converter/grease_converter/ops.py
@@ -0,0 +1,20 @@
+import bpy
+
+
+
+
+# ---------REGISTER ----------.
+
+classes = [
+]
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+
+def unregister():
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
+
+
