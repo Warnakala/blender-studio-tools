01e201e1d715d1a6c42bfb2ca23bc033df29349f
Paul Golter <paul@blender.org>
2022-02-08 15:40:33 +0100

    Add util module with redraw_ui() function

---
+++ blender_studio_pipeline/asset_pipeline/util.py
@@ -0,0 +1,10 @@
+import bpy
+
+
+def redraw_ui() -> None:
+    """
+    Forces blender to redraw the UI.
+    """
+    for screen in bpy.data.screens:
+        for area in screen.areas:
+            area.tag_redraw()
