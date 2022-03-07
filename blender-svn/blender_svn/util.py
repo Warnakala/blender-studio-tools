0d5fb6abced5d54eb7c91acde4a4a75b4fd43098
Paul Golter <paul@blender.org>
2022-03-07 15:12:16 +0100

    blender-svn: add util module.

---
+++ blender-svn/blender_svn/util.py
@@ -0,0 +1,53 @@
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# ***** END GPL LICENCE BLOCK *****
+#
+# (c) 2021, Blender Foundation - Paul Golter
+
+from typing import List, Dict, Union, Any, Set, Optional, Tuple, Generator
+
+import bpy
+
+
+def redraw_ui() -> None:
+    """
+    Forces blender to redraw the UI.
+    """
+    for screen in bpy.data.screens:
+        for area in screen.areas:
+            area.tag_redraw()
+
+
+def get_addon_prefs() -> bpy.types.AddonPreferences:
+    return bpy.context.preferences.addons[__package__].preferences
+
+
+def is_file_saved() -> bool:
+    return bool(bpy.data.filepath)
+
+
+def traverse_collection_tree(
+    collection: bpy.types.Collection,
+) -> Generator[bpy.types.Collection, None, None]:
+    yield collection
+    for child in collection.children:
+        yield from traverse_collection_tree(child)
+
+
+def del_collection(collection: bpy.types.Collection) -> None:
+    collection.user_clear()
+    bpy.data.collections.remove(collection)
