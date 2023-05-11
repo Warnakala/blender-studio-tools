a4631a8aec6be7daad6d8b457e884099fc3646e9
Nick Alberelli <nick@blender.org>
2023-05-11 13:41:09 -0400

    [Blender-Crawl] Update setup.py to use Blender_Crawl name

--- scripts/blender-crawl/setup.py
+++ scripts/blender-crawl/setup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# TODO Put the default_scripts in /usr/share and adjust main Logic to accomodate
+
 """The setup script."""
 
 from setuptools import setup, find_packages
@@ -7,8 +9,6 @@ from setuptools import setup, find_packages
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open("HISTORY.md") as history_file:
-    history = history_file.read()
 
 requirements = []
 
@@ -17,11 +17,11 @@ setup_requirements = []
 test_requirements = []
 
 setup(
-    author="Paul Golter",
-    author_email="paul@blender.org",
+    author="Nick Alberelli",
+    author_email="nick@blender.org",
     python_requires=">=3.5",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
@@ -30,17 +30,17 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    description="Command line tool to perform recursive purge of blend files in the console",
+    description="Command line tool to perform recursive crawl blend files from the console",
     install_requires=requirements,
-    long_description=readme + "\n\n" + history,
+    long_description=readme,
     include_package_data=True,
-    keywords="blender_purge",
-    name="blender_purge",
-    packages=find_packages(include=["blender_purge", "blender_purge.*"]),
+    keywords="blender_crawl",
+    name="blender_crawl",
+    packages=find_packages(include=["blender_crawl", "blender_crawl.*"]),
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
     version="0.1.0",
     zip_safe=False,
-    entry_points={"console_scripts": ["bpurge = blender_purge.__main__:main"]},
+    entry_points={"console_scripts": ["blender_crawl = blender_crawl.__main__:main"]},
 )
