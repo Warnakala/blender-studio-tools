08291ee5d93b723736949303b229421f24135272
Francesco Siddi <francesco.siddi@gmail.com>
2023-05-16 02:11:11 +0200

    Docs: Style and copy updates
    - Add Heebo font
    - Add hero banner image
    - Update tagline for website

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,5 @@
-import DefaultTheme from 'vitepress/theme'
+import DefaultTheme from 'vitepress/theme-without-fonts'
+import '@fontsource/heebo/variable.css'
 import './custom.css'
 
 export default DefaultTheme
